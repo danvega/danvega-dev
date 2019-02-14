2c9be499e004b2dc2f2c21db3149399624b07883
Dan Vega <danvega@gmail.com>
2019-02-13 22:05:41 -0500

    Updating the blog listing page to include some information about what this page is

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,14 +1,18 @@
 <template>
   <Layout>
-    <div class="articles">
-      <div v-for="post in $page.posts.edges" :key="post.node.id" class="article content">
-        <h2 class="title is-3">
-          <g-link :to="post.node.path">{{post.node.title}}</g-link>
-        </h2>
-        <small>{{ new Date(post.node.date).toLocaleDateString() }} • ☕️ {{post.node.timeToRead}} min read</small>
-        <p v-if="post.node.excerpt">{{post.node.excerpt}}</p>
-      </div>
-    </div>
+        <div class="content">
+          <h1 class="title is-2">Recent Articles</h1>
+          <p>I like to write about anything thta I am currently working or something new that interests me. If you would like me to write about something or be a guest blogger on your blog please reach out to me on <a href="https://twitter.com/therealdanvega">Twitter</a>.</p>
+          <div class="articles">
+            <div v-for="post in $page.posts.edges" :key="post.node.id" class="article content">
+              <h2 class="title is-3">
+                <g-link :to="post.node.path">{{post.node.title}}</g-link>
+              </h2>
+              <small>{{ new Date(post.node.date).toLocaleDateString() }} • ☕️ {{post.node.timeToRead}} min read</small>
+              <p v-if="post.node.excerpt">{{post.node.excerpt}}</p>
+            </div>
+          </div>
+        </div>
   </Layout>
 </template>
 
