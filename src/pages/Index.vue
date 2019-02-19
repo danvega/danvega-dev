27820f094d2f427931e7c183f030b6e3d244c335
Dan Vega <dan@techelevator.com>
2019-02-18 21:47:32 -0500

    Updating home page text

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,9 +5,8 @@
       <div class="columns is-centered">
         <div class="column is-two-thirds">
           <h1 class="title is-2">Hi, I’m Dan Vega.</h1>
-          <p>I am a Software Engineer living just outside of one of my favorite places in the world, Cleveland, Ohio. I am a self-taught programmer who firmly believes that you can accomplish anything in this life if you put your mind to it, roll up your sleeves and are willing to put in the work.</p>
-          <p>I have been writing software for over 20+ years now and I use my knowledge, passion, and influence to help other developers achieve their goals. I teach online and currently, have over 65,000 students.</p>
-          <p>In my personal life I am a husband to my best friend in the world and a father to a little princess. I enjoy reading, running and lifting weights to get me through the week.</p>
+          <p>I'm a programmer, curriculum developer and life long learner from Cleveland Ohio. </p>
+          <p>I created this site so that I could document the things that I learn or enjoy working with and help you avoid the mistakes that I make. If you enjoy my content please share it with your friends. </p>
         </div>
         <div class="column right-side">
           <g-image src="../assets/img/danvega-avatar.png" class="avatar" alt="Dan Vega Avatar"/>
