502054a14220b5a89cb514cae143ba402e138d9f
Dan Vega <danvega@gmail.com>
2019-03-12 17:11:14 -0400

    Updating new post script

--- scripts/newpost.js
+++ scripts/newpost.js
@@ -1,15 +1,23 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const log = console.log;
+const slugify = require('@sindresorhus/slugify')
+const jsToYaml = require('json-to-pretty-yaml')
+const prettier = require('prettier')
 const chalk = require('chalk');
+const log = console.log;
 const error = chalk.bold.red;
 const success = chalk.bold.green.inverse;
+
 
 (async () => {
   const args = process.argv;
   let metaData = null;
   if (args.length < 3) {
-    metaData = await inquirer.prompt([{
+    const {
+      title,
+      excerpt,
+      tags
+    } = await inquirer.prompt([{
         type: 'input',
         name: 'title',
         message: 'Post Title:',
@@ -20,64 +28,39 @@ const success = chalk.bold.green.inverse;
         message: 'Post Excerpt:'
       }, {
         type: 'input',
-        name: 'createdOn',
-        message: 'Post Date:',
-        default: new Date()
-      }, {
-        type: 'input',
         name: 'tags',
-        message: 'Tags:'
+        message: 'Tags (comma separated):'
       }
     ])
 
-    metaData.slug = slugify(metaData.title)
-
-    const year = metaData.createdOn.getFullYear()
-    const month = `${metaData.createdOn.getMonth() + 1 < 10 ? "0" : ""}${metaData.createdOn.getMonth() + 1}`
-    const day = `${metaData.createdOn.getDate() < 10 ? "0" : ""}${metaData.createdOn.getDate()}`
+    const slug = slugify(title)
+    const createdOn = new Date()
+    const year = createdOn.getFullYear()
+    const month = `${createdOn.getMonth() + 1 < 10 ? "0" : ""}${createdOn.getMonth() + 1}`
+    const day = `${createdOn.getDate() < 10 ? "0" : ""}${createdOn.getDate()}`
     const blogPostFolder = `./blog/${year}/${month}/${day}`;
+    const listify = a => a && a.trim().length ? a.split(',').map(s => s.trim()).filter(Boolean) : null
 
     if (!fs.existsSync(blogPostFolder)) {
       fs.mkdirSync(blogPostFolder);
     }
 
-    if (!fs.existsSync(`${blogPostFolder}/${metaData.slug}.md`)) {
-      fs.writeFileSync(
-        `${blogPostFolder}/${metaData.slug}.md`,
-        `---
-slug: ${metaData.slug}
-title: ${metaData.title}
-date: ${metaData.createdOn.toISOString()}
-published: false
-excerpt:  ${metaData.excerpt}
-tags: ${metaData.tags}
-cover: 
----
-`,
-        (err) => {
-          if (err) {
-            return error(err)
-          }
-        }
-      )
+    const yaml = jsToYaml.stringify({
+      slug,
+      title,
+      date: createdOn.toISOString(),
+      published: false,
+      excerpt: excerpt,
+      author: 'Dan Vega',
+      tags: listify(tags),
+      cover: ''
+    })
 
-      success(`Post ${metaData.title} was created successfully`)
-    }
+    const markdown = prettier.format(`---\n${yaml}\n---\n`, {
+      parser: 'markdown',
+    })
+    fs.writeFileSync(`${blogPostFolder}/${slug}.md`, markdown)
 
-
+    success(`Post ${title} was created successfully`)
   }
 })()
-
-// thank you - https://medium.com/@mhagemann/the-ultimate-way-to-slugify-a-url-string-in-javascript-b8e4a0d849e1
-function slugify(string) {
-  const a = 'àáäâãåèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;'
-  const b = 'aaaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------'
-  const p = new RegExp(a.split('').join('|'), 'g')
-  return string.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with ‘and’
-    .replace(/[^\w\-]+/g, '') // Remove all non-word characters
-    .replace(/\-\-+/g, '-') // Replace multiple — with single -
-    .replace(/^-+/, '') // Trim — from start of text .replace(/-+$/, '') // Trim — from end of text
-}
