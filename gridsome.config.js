0a97618f7b091dafc792ef1f50592e382ba989b9
Dan Vega <danvega@gmail.com>
2019-03-15 23:25:26 -0400

    Adding RSS feed for my blog

--- gridsome.config.js
+++ gridsome.config.js
@@ -37,11 +37,40 @@ module.exports = {
       }
     },
     {
+      use: 'gridsome-plugin-rss',
+      options: {
+        contentTypeName: 'Post',
+        feedOptions: {
+          title: 'Dan Vega',
+          feed_url: 'https://danvega.dev/rss.xml',
+          site_url: 'https://danvega.dev'
+        },
+        feedItemOptions: node => ({
+          title: node.title,
+          description: node.excerpt,
+          url: getPostURL(node.fields.date, node.slug),
+          author: node.fields.author
+        }),
+        output: {
+          dir: './static',
+          name: 'rss.xml'
+        }
+      }
+    },
+    {
       use: '@gridsome/plugin-google-analytics',
       options: {
         id: 'UA-133826656-1'
       }
     }
   ]
+}
 
+function getPostURL(date, slug) {
+  const createdOn = new Date(date);
+  console.log(createdOn);
+  const year = createdOn.getFullYear()
+  const month = `${createdOn.getMonth() + 1 < 10 ? "0" : ""}${createdOn.getMonth() + 1}`
+  const day = `${createdOn.getDate() < 10 ? "0" : ""}${createdOn.getDate()}`
+  return `https://www.danvega.dev/blog/${year}/${month}/${day}/${slug}`;
 }
