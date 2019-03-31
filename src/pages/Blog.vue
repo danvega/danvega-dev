3d32c22a8b8afca47ea7a2c8f388d3573d68c979
Dan Vega <danvega@gmail.com>
2019-03-31 10:35:29 -0400

    Fixing pagination to support a lot more pages

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -2,8 +2,11 @@
   <Layout>
     <div class="content">
       <h1 class="title is-2">Recent Articles</h1>
-      <p>I like to write about anything thta I am currently working or something new that interests me. If you would like me to write about something or be a guest blogger on your blog please reach out to me on
-        <a href="https://twitter.com/therealdanvega">Twitter</a>.
+      <p>
+        I like to write about anything thta I am currently working or something new that interests me. If you would like me to write about something or be a guest blogger on your blog please reach out to me on
+        <a
+          href="https://twitter.com/therealdanvega"
+        >Twitter</a>.
       </p>
       <div class="articles">
         <div v-for="post in $page.posts.edges" :key="post.node.id" class="article content">
@@ -17,7 +20,9 @@
     </div>
     <BulmaPagination
       baseUrl="/blog"
-      :info="$page.posts.pageInfo"
+      :currentPage="$page.posts.pageInfo.currentPage"
+      :totalPages="$page.posts.pageInfo.totalPages"
+      :maxVisibleButtons="5"
       v-if="$page.posts.pageInfo.totalPages > 1"
     />
   </Layout>
