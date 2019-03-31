1c17b3690b111fe8fdd74672ebdc4a86307c9124
Dan Vega <danvega@gmail.com>
2019-03-31 05:44:08 -0400

    Small change to the home page'

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
       <div class="columns is-centered">
         <div class="column is-two-thirds">
           <h1 class="title is-2">Hi, I’m Dan Vega.</h1>
-          <p>I'm a programmer, curriculum developer and life long learner from Cleveland Ohio.</p>
+          <p>I'm a Software Developer, Curriculum Developer and life long learner from Cleveland Ohio.</p>
           <p>I created this site so that I could document the things that I learn or enjoy working with and help you avoid the mistakes that I make. If you enjoy my content please share it with your friends.</p>
         </div>
         <div class="column right-side">
