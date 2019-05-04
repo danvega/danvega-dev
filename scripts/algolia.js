64c403f50c04f12de470d1c728ea0b9185976b37
Dan Vega <danvega@gmail.com>
2019-05-03 21:52:42 -0400

    working on the script to add an objectID and modifiedON to each blog post

---
+++ scripts/algolia.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const readdirp = require('readdirp')
+const chalk = require('chalk')
+const log = console.log
+const error = chalk.bold.red
+const success = chalk.bold.green.inverse
+
+const replace = require('replace-in-file');
+const yamlFront = require('yaml-front-matter');
+const YAML = require('json-to-pretty-yaml');
+
+let posts = [];
+readdirp('./blog', {
+    fileFilter: '*.md'
+  })
+  .on('data', (entry) => {
+    posts.push(entry.fullPath);
+  })
+  .on('end', () => {
+    updateFrontMatter();
+  })
+
+function updateFrontMatter() {
+
+  fs.readFile(post, {}, (err, data) => {
+    const fm = yamlFront.loadFront(data);
+    fm['objectID'] = '123456789';
+    fm['modifiedOn'] = fm.date;
+    delete fm.__content;
+
+    const frontMatter = YAML.stringify(fm);
+    const regex = new RegExp('---[^)]+---', 'i');
+    const updatedFrontMatter = '---\n' + frontMatter + '---\n'
+
+    const options = {
+      files: posts,
+      from: regex,
+      to: updatedFrontMatter
+    };
+    try {
+      const results = replace.sync(options);
+      log(success('Replacement results:', results))
+    } catch (error) {
+      log(error('Error occurred:', error));
+    }
+
+  })
+
+
+
+}
---
+++ scripts/example.md
@@ -0,0 +1,14 @@
+---
+slug: "gridsome-codesandbox-plugin"
+title: "How I created a CodeSandbox plugin for Gridsome"
+date: "2019-05-02T15:30:43.839Z"
+modified: "2019-05-02T15:30:43.839Z"
+published: true
+excerpt: "In this article, I am going to introduce you to CodeSandbox and tell you a little bit about why I love it so much"
+author: "Dan Vega"
+tags: ["vue", "gridsome", "codesandbox"]
+cover: ./gridsome-codesandbox-plugin-cover.png
+---
+
+Some Other content
+
