67ce514ac6663ca183390222fe462390eecc4009
Dan Vega <danvega@gmail.com>
2019-05-06 09:23:16 -0400

    Subscribe and thank you page

---
+++ src/pages/SubscribeThankYou.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <div class="content">
+      <h1 class="title is-2">Thank you for subscribing</h1>
+      <p>Important – Check Your Email to Verify</p>
+      <p>An email has been sent to you that contains a verification link. Please click the “Confirm Subscription” link to complete your subscription.</p>
+      <p>Please check your email now to find the confirmation email. If you don’t see it check your spam filtering system.</p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  name: "subscribe-thank-you"
+};
+</script>
