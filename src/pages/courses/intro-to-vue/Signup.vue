266d4695e89a37ccc8d50b0b01ae48211211be9a
Dan Vega <danvega@gmail.com>
2019-05-16 22:26:11 -0400

    Adding intro to VueJS signup page

---
+++ src/pages/courses/intro-to-vue/Signup.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="container">
+    <header>
+      <div class="background-overlay"></div>
+      <div class="logo">
+        <g-image src="../../../assets/img/danvega-avatar.png" width="80"/>
+      </div>
+    </header>
+    <div class="content">
+      <div class="about">
+        <h1>FREE Introduction to VueJS Course</h1>
+        <h2>Do you want to learn how to get up and running with VueJS in just 1 hour?</h2>
+        <p>
+          If you're interested in getting started with VueJS I have a free course that is really going to help you. With over 75,000 students worldwide
+          and lots of metrics to back it up I have found that developers take courses to learn a specific topic.
+        </p>
+        <p>
+          In this course I will explain to you what VueJS is and why it's a great choice for beginners. After that you will get your development
+          enviornment setup and write your first application. The best part about this course is that you only need an hour to get up and running.
+        </p>
+        <p>
+          If you're interested in learning more you can check out
+          <a
+            href="https://www.danvega.dev/blog/2019/04/30/up-and-running-with-vue"
+          >this article</a> I wrote on how to get started with Vue.
+          If you have any questions please feel free to reach out to me on
+          <a
+            href="https://twitter.com/therealdanvega"
+          >Twitter</a>.
+        </p>
+      </div>
+      <div class="signup">
+        <div id="convertkit"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {};
+  },
+  methods: {
+    loadSignupForm() {
+      const converkit = document.createElement("script");
+      converkit.setAttribute(
+        "src",
+        "https://f.convertkit.com/99df6613cd/581d8fda8f.js"
+      );
+      converkit.setAttribute("data-uid", "99df6613cd");
+      converkit.setAttribute("async", true);
+      document.getElementById("convertkit").appendChild(converkit);
+    }
+  },
+  mounted() {
+    this.loadSignupForm();
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+/*
+body {
+  background-color: #33506e;
+  background-image: linear-gradient(rgba(79, 79, 79, .2), transparent);
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  font-family: "IBM Plex Sans", sans-serif;
+  min-height: calc(100vh - 140px);
+  padding: 60px 0 170px;
+} */
+
+body {
+  background-image: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  /* background-image: linear-gradient(to top, #48c6ef 0%, #6f86d6 100%); */
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  color: white;
+}
+
+.background-overlay {
+  pointer-events: none;
+  mix-blend-mode: overlay;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  position: absolute;
+  background-image: url(~@/assets/img/course_signup_bg.png);
+  background-size: cover;
+}
+
+.container {
+  width: 75vw;
+  height: 100vh;
+  margin: auto;
+}
+
+.logo {
+  display: flex;
+  margin-top: 100px;
+}
+
+.content {
+  margin-top: 80px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.about {
+  width: 50%;
+}
+
+.about h1 {
+  box-sizing: border-box;
+  color: white !important;
+  border-bottom: 1px solid white;
+}
+
+.about h2 {
+  box-sizing: border-box;
+  color: white !important;
+  margin-bottom: 40px;
+  font-size: 19px;
+}
+
+.about p {
+  box-sizing: border-box;
+  font-family: "IBM Plex Sans", sans-serif;
+  line-height: 2.1;
+  margin-bottom: 30px;
+}
+.about a,
+.about a:visited {
+  color: white;
+}
+.signup {
+  justify-content: center;
+  align-content: center;
+  text-align: center;
+}
+</style>
