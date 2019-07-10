a7f84e9fd529b241acb9dbd14906e13a92e465f7
Dan Vega <dan@techelevator.com>
2019-07-10 16:15:49 -0400

    Create a json structure of all posts to use as the basis for search

--- gridsome.server.js
+++ gridsome.server.js
@@ -2,11 +2,44 @@
 // on server-side and add custom data to the GraphQL data layer.
 // Learn more: https://gridsome.org/docs/server-api
 
-// Changes here requires a server restart.
+// Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-module.exports = function (api) {
+const fs = require('fs');
+const path = require('path');
+const pick = require('lodash.pick');
+
+module.exports = function (api, options) {
   api.loadSource(store => {
     // Use the Data store API here: https://gridsome.org/docs/data-store-api
+  })
+
+  api.beforeBuild(({ config, store }) => {
+
+    // Generate an index file for Fuse to search Posts
+    const { collection } = store.getContentType('Post');
+
+    const posts = collection.data.map(post => {
+      return pick(post, ['title', 'path', 'excerpt']);
+    });
+
+    const output = {
+      dir: './static',
+      name: 'search.json',
+      ...options.output
+    }
+
+    const outputPath = path.resolve(process.cwd(), output.dir)
+    const outputPathExists = fs.existsSync(outputPath)
+    const fileName = output.name.endsWith('.json')
+      ? output.name
+      : `${output.name}.json`
+
+    if (outputPathExists) {
+      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
+    } else {
+      fs.mkdirSync(outputPath)
+      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
+    }
   })
 }
