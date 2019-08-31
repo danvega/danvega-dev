52a1023208bb5da2f31420dd4f677cb10d63e785
Dan Vega <danvega@gmail.com>
2019-08-31 07:15:08 -0400

    Removing sentury endpoint which was hardcoded and shouldn't be because others who clone this template then report errors to my log

--- src/main.js
+++ src/main.js
@@ -2,38 +2,36 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 // Import main css
-import '~/assets/style/index.scss';
+import "~/assets/style/index.scss";
 
-import DefaultLayout from '~/layouts/Default.vue'
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
-import VueFuse from 'vue-fuse'
+import DefaultLayout from "~/layouts/Default.vue";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
+import VueFuse from "vue-fuse";
 
-export default function (Vue, {
-  router,
-  head,
-  isClient
-}) {
+export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component('Layout', DefaultLayout)
-  Vue.use(VueFuse)
+  Vue.component("Layout", DefaultLayout);
+  Vue.use(VueFuse);
 
   Sentry.init({
-    dsn: 'https://506350377fb44e1fbc756457c784e23f@sentry.io/1476538',
-    integrations: [new Integrations.Vue({
-      Vue,
-      attachProps: true
-    })],
-  })
+    dsn: process.env.SENTURY_DSN,
+    integrations: [
+      new Integrations.Vue({
+        Vue,
+        attachProps: true
+      })
+    ]
+  });
 
   head.link.push({
-    rel: 'stylesheet',
-    href: 'https://use.fontawesome.com/releases/v5.7.0/css/all.css'
-  })
+    rel: "stylesheet",
+    href: "https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+  });
 
   head.link.push({
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css?family=Oswald|Permanent+Marker&display=swap'
-  })
-
+    rel: "stylesheet",
+    href:
+      "https://fonts.googleapis.com/css?family=Oswald|Permanent+Marker&display=swap"
+  });
 }
