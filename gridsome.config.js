7b4f38938e1ecf6b75696b363c7f9075321e6c22
Dan Vega <danvega@gmail.com>
2019-09-27 23:10:14 -0400

    Updating the config to include the twitter plugin for the newsletter

--- gridsome.config.js
+++ gridsome.config.js
@@ -9,54 +9,59 @@ module.exports = {
   siteUrl: "https://www.danvega.dev",
   siteDescription: "Person blog of Dan Vega",
   icon: "src/img/danvega-favicon.png",
-  plugins: [{
-      use: '@gridsome/source-filesystem',
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'blog/**/*.md',
-        typeName: 'Post',
-        route: '/blog/:year/:month/:day/:slug',
+        path: "blog/**/*.md",
+        typeName: "Post",
+        route: "/blog/:year/:month/:day/:slug",
         refs: {
           tags: {
-            typeName: 'Tag',
-            route: '/tag/:slug',
+            typeName: "Tag",
+            route: "/tag/:slug",
             create: true
           }
         },
         resolveAbsolutePaths: true,
         remark: {
-          autolinkClassName: 'fas fa-hashtag',
-          externalLinksTarget: '_blank',
-          externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+          autolinkClassName: "fas fa-hashtag",
+          externalLinksTarget: "_blank",
+          externalLinksRel: ["nofollow", "noopener", "noreferrer"],
           plugins: [
-            ['gridsome-plugin-remark-shiki', {
-              theme: 'nord'
-            }],
-            ['gridsome-plugin-remark-twitter', {}],
-            ['gridsome-plugin-remark-codesandbox', {}]
+            [
+              "gridsome-plugin-remark-shiki",
+              {
+                theme: "nord"
+              }
+            ],
+            ["gridsome-plugin-remark-twitter", {}],
+            ["gridsome-plugin-remark-codesandbox", {}]
           ]
         }
       }
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        path: 'newsletter/**/*.md',
-        typeName: 'Newsletter',
-        route: '/newsletter/:year/:month/:day/:slug',
+        path: "newsletter/**/*.md",
+        typeName: "Newsletter",
+        route: "/newsletter/:year/:month/:day/:slug",
         resolveAbsolutePaths: true,
         remark: {
-          autolinkHeadings: false
+          autolinkHeadings: false,
+          plugins: [["gridsome-plugin-remark-twitter", {}]]
         }
       }
     },
     {
-      use: 'gridsome-plugin-rss',
+      use: "gridsome-plugin-rss",
       options: {
-        contentTypeName: 'Post',
+        contentTypeName: "Post",
         feedOptions: {
-          title: 'Dan Vega',
-          feed_url: 'https://danvega.dev/rss.xml',
-          site_url: 'https://danvega.dev'
+          title: "Dan Vega",
+          feed_url: "https://danvega.dev/rss.xml",
+          site_url: "https://danvega.dev"
         },
         feedItemOptions: node => ({
           title: node.title,
@@ -64,41 +69,45 @@ module.exports = {
           url: getPostURL(node.date, node.slug),
           author: node.author,
           date: node.date,
-          custom_elements: [{
-            published: node.date.toString(),
-          }, ]
+          custom_elements: [
+            {
+              published: node.date.toString()
+            }
+          ]
         }),
         output: {
-          dir: './static',
-          name: 'rss.xml'
+          dir: "./static",
+          name: "rss.xml"
         }
       }
     },
     {
-      use: '@gridsome/plugin-sitemap',
+      use: "@gridsome/plugin-sitemap",
       options: {
         cacheTime: 600000, // default
         config: {
-          '/blog/*': {
-            changefreq: 'daily',
+          "/blog/*": {
+            changefreq: "daily",
             priority: 0.5
           }
         }
       }
     },
     {
-      use: '@gridsome/plugin-google-analytics',
+      use: "@gridsome/plugin-google-analytics",
       options: {
-        id: 'UA-133826656-1'
+        id: "UA-133826656-1"
       }
     }
   ]
-}
+};
 
 function getPostURL(date, slug) {
   const createdOn = new Date(date);
-  const year = createdOn.getFullYear()
-  const month = `${createdOn.getMonth() + 1 < 10 ? "0" : ""}${createdOn.getMonth() + 1}`
-  const day = `${createdOn.getDate() < 10 ? "0" : ""}${createdOn.getDate()}`
+  const year = createdOn.getFullYear();
+  const month = `${
+    createdOn.getMonth() + 1 < 10 ? "0" : ""
+  }${createdOn.getMonth() + 1}`;
+  const day = `${createdOn.getDate() < 10 ? "0" : ""}${createdOn.getDate()}`;
   return `https://www.danvega.dev/blog/${year}/${month}/${day}/${slug}`;
 }
