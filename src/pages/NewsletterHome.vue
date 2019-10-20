944c2564f110bb79137856397ac30793fa4a7a99
Dan Vega <danvega@gmail.com>
2019-10-20 08:09:41 -0400

    fixing newsletter issues

--- src/pages/NewsletterHome.vue
+++ src/pages/NewsletterHome.vue
@@ -1,20 +1,15 @@
 <template>
   <Layout>
     <div class="content">
-      <h1 class="title is-2">Newsletter Signup</h1>
+      <h1 class="title is-2">Newsletter</h1>
       <p>If you would like to stay up to date with everything that I am up to you are in the right place. If you signup below I will send you fresh content, tips and tricks and let you know what articles & courses I am working on.</p>
-      <convert-kit uid="9e7e6036d3" script="https://f.convertkit.com/9e7e6036d3/e146cc5eb6.js"></convert-kit>
     </div>
   </Layout>
 </template>
 
 <script>
-import ConvertKit from "@/components/ConvertKit";
-
 export default {
-  name: "signup",
-  components: {
-    ConvertKit
-  }
+  name: "newsletterclear",
+  components: {}
 };
 </script>
