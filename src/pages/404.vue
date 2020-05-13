93b5299077f7aa46be2dec9f44d83d9acea95a04
Dan Vega <danvega@gmail.com>
2020-05-12 23:27:39 -0400

    updating the 404 page

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,6 +1,11 @@
 <template>
   <Layout>
-    <main>This is not the page you are looking for.</main>
+    <h1>Ooops: Page Not Found</h1>
+    <p>
+      It looks like you have
+      <strong>come up empty</strong> my friend. It happens once in awhile so don't feel bad about it. The good news for you is that you can use the navigation above to find what you're looking for. If you are looking for a specifc blog post please try using the search. If nothing works and you still need please feel free to contact me.
+    </p>
+    <g-image src="@/assets/img/undraw_empty_xct9.png" />
   </Layout>
 </template>
 
@@ -9,4 +14,7 @@ export default {};
 </script>
 
 <style scoped>
+img {
+  max-width: 100%;
+}
 </style>
