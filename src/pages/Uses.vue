13086a0e372ce42cb018d71392781e9d24869865
Dan Vega <danvega@gmail.com>
2020-05-18 21:39:49 -0400

    adding a uses page

---
+++ src/pages/Uses.vue
@@ -0,0 +1,254 @@
+<template>
+  <Layout>
+    <h1>Uses</h1>
+    <p>
+      I am often asked about the gear I use as a software developer and content creator. The hardware and software I use is changing all the time so this page will serve as a place for an updated list. If you have a question about anything I use or a suggestion for something I am missing please reach out to me on
+      <a
+        href="https://www.twitter.com/therealdanvega"
+        aria-label="Dan Vega on Twitter"
+      >Twitter</a>. The inspiration for this page came from
+      <a
+        href="https://wesbos.com/uses"
+        aria-label="Wes Bos Uses"
+      >Wes Bos</a>'s uses page and has been submitted to
+      <a
+        href="https://uses.tech/"
+        aria-label="uses.tech"
+      >uses.tech</a>.
+    </p>
+    <h2>Editor + Terminal</h2>
+    <ul>
+      <li>
+        <a href="https://code.visualstudio.com/" aria-label="Visual Studio Code">Visual Studio Code</a> is my current editor and I use it for pretty much everything.
+      </li>
+      <li>
+        The theme I am currently using is
+        <a
+          href="https://marketplace.visualstudio.com/items?itemName=RobbOwen.synthwave-vscode"
+          aria-label="Synthwave 84 by Robb Owen"
+        >Synthwave 84</a> by Robb Owen.
+      </li>
+      <li>
+        Font is
+        <a href="https://sourcefoundry.org/hack/" aria-label="Hack Font">Hack</a>
+      </li>
+      <li>
+        <a href="https://www.iterm2.com/" aria-label="iTerm">iTerm2</a> is my Terminal
+      </li>
+    </ul>
+    <g-image src="@/assets/img/uses/vscode.png" alt="My Visual Studio Code Setup" />
+
+    <h2>Java & Spring Development</h2>
+    <ul>
+      <li>
+        <a href="https://www.jetbrains.com/idea/" aria-label="IntelliJ IDEA">IntelliJ IDEA</a> Ultimate Eiditon is my IDE of choice for Java & Spring Development
+      </li>
+      <li>Theme is IntelliJ Light</li>
+      <li>
+        Font is
+        <a
+          href="https://www.jetbrains.com/lp/mono/"
+          aria-label="JetBrains Mono Font"
+        >JetBrains Mono</a>
+      </li>
+    </ul>
+    <g-image src="@/assets/img/uses/intellij.png" alt="My IntelliJ IDEA Setup" />
+
+    <h2>Apps</h2>
+    <ul>
+      <li>Google Apps (Domains, Mail, Docs, Spreadsheets, etc...)</li>
+      <li>Google Chrome as my main web browser</li>
+      <li>
+        <a href="https://www.notion.so" aria-label="Notion">Notion</a> I use Notion for almost everything and it is one of those tools I couldn't live without.
+      </li>
+      <li>
+        I use
+        <a
+          href="https://www.adobe.com/products/xd.html"
+          aria-label="Adobe XD Product Page"
+        >Adobe XD</a> for designing websites, YouTube thumbnails, blog post covers and any other reason I can find to use it.
+      </li>
+      <li>iTunes Music - I live in the Apple ecosystem so having all of my music in one place that I can access on my phone, tv and watch is important.</li>
+      <li>
+        I listen to all of my podcasts on
+        <a
+          href="https://www.pocketcasts.com"
+          aria-label="Pocket Casts Podcast Player"
+        >Pocket Casts</a>.
+      </li>
+      <li>
+        I love
+        <a href="http://discord.com/" aria-label="Discord Communities">Discord</a> for running
+        <a
+          href="https://discord.gg/gx4Z8XY"
+          aria-label="Join my Discord community"
+        >my community</a> and being a part of so many great ones.
+      </li>
+    </ul>
+
+    <h2>Recording</h2>
+    <ul>
+      <li>
+        I use
+        <a
+          href="https://support.apple.com/guide/mac-help/take-screenshots-or-screen-recordings-mh26782/mac"
+          aria-label="macOS Screenshot Utility"
+        >macOS Screenshot Utility</a> to record what is on my screen.
+      </li>
+      <li>
+        When I am recording screencasts I use the
+        <a
+          href="https://amzn.to/3dWTkwX"
+          aria-label="Audio Technica AT2020"
+        >Audio-Technica AT2020 Microphone</a> to record audio.
+      </li>
+      <li>
+        Speaking of Audio.. I ❤️ my
+        <a
+          href="https://amzn.to/2LHbZAK"
+          aria-label="Beats Headphones"
+        >Beats Solo</a> wireless headphones.
+      </li>
+      <li>
+        I use a
+        <a href="https://amzn.to/36cuV43" aria-label="Canon 70D Camera">Canon 70D</a> with a
+        <a href="https://amzn.to/2ZkquCD" aria-label="24mm f/2.8 Lens">24mm f/2.8 STM Lens</a> for all of my a-roll.
+      </li>
+      <li>
+        When I am filming a-roll I use the
+        <a
+          href="https://amzn.to/2TgW54v"
+          aria-label="Rode VideoMic Pro"
+        >Rode VideoMic Pro</a> to capture audio.
+      </li>
+      <li>
+        I use the
+        <a
+          href="https://amzn.to/2X7XzPF"
+          aria-label="Fovitec StudioPro Lights"
+        >Fovitec StudioPro lights</a> for ligthing when I need it. These are a bit large for my small studio so I am looking at picking up a couple of the
+        <a
+          href="https://amzn.to/2X3qPXG"
+          aria-label="Elgato Key Lights"
+        >Elgato Key Lights</a>
+      </li>
+      <li>
+        My webcam is the
+        <a
+          href="https://amzn.to/3g3zKBa"
+          aria-label="Logitech C920 Webcam"
+        >Logitech HD Pro C920</a>
+      </li>
+      <li>
+        I use
+        <a
+          href="https://www.adobe.com/products/premiere.html"
+          aria-label="Aodbe Premiere Pro"
+        >Adobe Premiere Pro</a> to edit all of my videos.
+      </li>
+      <li>
+        I have a subscription to
+        <a
+          href="https://www.epidemicsound.com/"
+          aria-label="Epidemic Sound"
+        >Epidemic Sound</a> for any audio needs.
+      </li>
+      <li>
+        I use
+        <a
+          href="https://www.ecamm.com/mac/ecammlive/"
+          aria-label="Ecamm Live Streaming Software"
+        >Ecamm Live</a> for Streaming.
+      </li>
+      <li>
+        I use
+        <a href="https://www.dropbox.com/" aria-label="Dropbox for Storage">Dropbox</a> for storage and backup.
+      </li>
+      <li>
+        When I need a green screen I use the
+        <a
+          href="https://amzn.to/3cKinmQ"
+          aria-label="Elgato Green Screen"
+        >Elgato Green Screen</a>. It collapses for easy storage and works great.
+      </li>
+    </ul>
+
+    <div class="center">
+      <g-image src="@/assets/img/uses/canon70d.jpg" alt="Canon 70D and 24mm Lens" />
+    </div>
+
+    <h2>Desk Setup</h2>
+    <ul>
+      <li>
+        <a href="https://amzn.to/2ZrkS9Z" aria-label="MacBook Pro">MacBook Pro</a> (Retina, 15-inch, Mid 2015) 2.2GHz Quad-Core Intel i7 / 16GB RAM
+      </li>
+      <li>
+        <a href="https://amzn.to/2Zrlbl9" aria-label="Laptop Stand">Bestand Laptop Stand</a>
+      </li>
+      <li>
+        <a href="https://amzn.to/3g2jiks" aria-label="Dell Ultrawide Monitor">Dell UltraSharp U3415W</a> 34" Curved Widescreen Monitor
+      </li>
+      <li>
+        <a href="https://amzn.to/2WFflKQ" aria-label="Apple Magic Keyboard">Apple Magic Keyboard</a>
+      </li>
+      <li>
+        <a href="https://amzn.to/36gFIKn" aria-label="Logitech Mouse">Logitech Mouse</a> - I really need to upgrade this.
+      </li>
+      <li>My speakers are embarasingly like 20 years old and I have no excuse 🤦‍♂️</li>
+      <li>
+        My desk is not actually a desk. It's the Ikea the
+        <a
+          href="https://www.ikea.com/us/en/p/karlby-countertop-walnut-veneer-70335212/"
+          aria-label="IKEA Countertop"
+        >Karlby countertop</a> on top of 2 cabinets hack that you have probably seen a million times by now. I really like this setup and it works well for my space.
+      </li>
+    </ul>
+
+    <div class="center">
+      <g-image src="@/assets/img/uses/desk-setup.jpg" alt="My desk setup" />
+    </div>
+
+    <h2>Work Setup</h2>
+    <p>I work from home a lot (full time at the time of writing this) so I put together another desk setup. I don't like having to switch out laptops everytime I move from work to personal so I like having a 2nd setup I can just sit down and get to work at.</p>
+    <ul>
+      <li>
+        I love the
+        <a
+          href="https://www.autonomous.ai/standing-desks/smartdesk-2-home"
+          aria-label="Standup Desk"
+        >Autonomous Smart Desk 2</a> which allows me to sit down and stand up when I need a break.
+      </li>
+      <li>
+        <a href="https://amzn.to/2ZrkS9Z" aria-label="MacBook Pro">MacBook Pro</a> (13-inch, 2017, Two Thunderbolt 3 Ports) 2.3GHz Quad-Core Intel i5 / 16GB RAM
+      </li>
+      <li>
+        <a href="https://amzn.to/3bJBaNy" aria-label="Monitor Arm">Amazon Basics Monitor Arm</a>
+      </li>
+      <li>
+        <a href="https://amzn.to/2zQUOtO" aria-label="Asus 27 Monitor">Asus 27" Monitor</a>
+      </li>
+      <li>
+        <a href="https://amzn.to/2Xfmfpw" aria-label="USB-C Hub">HooToo USB C Hub</a> for connecting all of my devices
+      </li>
+    </ul>
+
+    <h2>Affiliate Links</h2>
+    <p>A few of the links on this page will earn me an affiliate commission. This comes at no extra cost to you and helps support me as a creator. Thank you 🙏</p>
+  </Layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+img {
+  max-width: 100%;
+}
+.center {
+  text-align: center;
+}
+p:last-child {
+  margin-bottom: 40px;
+}
+</style>
