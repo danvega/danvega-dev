598ca0d52483c41b14c45940a736dd1af695114c
Dan Vega <danvega@gmail.com>
2021-09-10 16:07:40 -0400

    updating about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,21 +2,87 @@
   <Layout>
     <main>
       <h1>Hi, I’m Dan Vega.</h1>
-      <img src="../assets/img/dv-headshot.jpg" alt="Dan Vega" class="headshot" />
-      <p>I am a Software Engineer living just outside of one of my favorite places in the world, Cleveland, Ohio. I am a self-taught programmer who firmly believes that you can accomplish anything in this life if you put your mind to it, roll up your sleeves and are willing to put in the work.</p>
-      <p>I have been writing software for over 20+ years now and I use my knowledge, passion, and influence to help other developers achieve their goals. I teach online and currently, have over 100,000 students.</p>
-      <p>In my personal life I am a husband to my best friend in the world and a father to a little princess. I enjoy reading, running and lifting weights to get me through the week.</p>
+      <img
+        src="../assets/img/dv-headshot.jpg"
+        alt="Dan Vega"
+        class="headshot"
+      />
+      <p>
+        I am a Software Engineer living just outside of one of my favorite
+        places in the world, Cleveland, Ohio. I am a self-taught programmer who
+        firmly believes that you can accomplish anything in this life if you put
+        your mind to it, roll up your sleeves and are willing to put in the
+        work.
+      </p>
+      <p>
+        I have been writing software for over 20+ years now and I use my
+        knowledge, passion, and influence to help other developers achieve their
+        goals. I teach online and currently, have over 100,000 students.
+      </p>
+      <p>
+        In my personal life I am a husband to my best friend in the world and a
+        father of two little girls. I enjoy reading, running and lifting weights
+        to get me through the week.
+      </p>
       <h2>My Journey</h2>
-      <p>My journey to becoming a computer programmer did not start out at a fancy university. I like to give my mom some credit for making me take a typing class in high school that I excelled in, which sparked my interest in computers, thanks, mom! After high school, I was working as a landscaper and one day I just decided that this was not the life for me. I decided to enroll at a local college for a programming certification in Applied Computer Sciences. Once I completed this certification, I did some freelancing to hone my skills, and soon after I landed my first job at a startup in San Francisco! It was difficult leaving my friends and family, but I was so excited for this opportunity and quickly packed my bags for the west coast.</p>
-      <p>My educational journey did not stop there. I like to consider myself a lifelong learner. I believe having a passion for learning really is the secret to success in life. While I was living in California, I hired a teaching assistant from UC Berkeley to tutor me in computer programming. I spent most of my nights far from home immersing myself in Java programming books to advance my skills, and it certainly paid off!</p>
-      <p>After two years in San Francisco, I landed a new position back in Cleveland and jumped at the opportunity to move back home. I continued to advance to different positions and companies over the years and here I am, 20+ years later, still loving to code every single day.</p>
+      <p>
+        My journey to becoming a computer programmer did not start out at a
+        fancy university. I like to give my mom some credit for making me take a
+        typing class in high school that I excelled in, which sparked my
+        interest in computers, thanks, mom! After high school, I was working as
+        a landscaper and one day I just decided that this was not the life for
+        me. I decided to enroll at a local college for a programming
+        certification in Applied Computer Sciences. Once I completed this
+        certification, I did some freelancing to hone my skills, and soon after
+        I landed my first job at a startup in San Francisco! It was difficult
+        leaving my friends and family, but I was so excited for this opportunity
+        and quickly packed my bags for the west coast.
+      </p>
+      <p>
+        My educational journey did not stop there. I like to consider myself a
+        lifelong learner. I believe having a passion for learning really is the
+        secret to success in life. While I was living in California, I hired a
+        teaching assistant from UC Berkeley to tutor me in computer programming.
+        I spent most of my nights far from home immersing myself in Java
+        programming books to advance my skills, and it certainly paid off!
+      </p>
+      <p>
+        After two years in San Francisco, I landed a new position back in
+        Cleveland and jumped at the opportunity to move back home. I continued
+        to advance to different positions and companies over the years and here
+        I am, 20+ years later, still loving to code every single day.
+      </p>
 
       <h2>One thing that has always stuck with me</h2>
-      <p>One thing that has always stuck with me over the years is how grateful I am for all of the people who taught and inspired me over the years. This help inspired me to always share my passion for learning with others. When I discovered online teaching, I knew this was my calling to help other people make positive changes in their lives through education. As I said in the beginning, I believe if you are willing to put in the work, you can accomplish anything in this life. If programming is what you are hoping to learn and work hard on, I am happy to show you the way!</p>
+      <p>
+        One thing that has always stuck with me over the years is how grateful I
+        am for all of the people who taught and inspired me over the years. This
+        help inspired me to always share my passion for learning with others.
+        When I discovered online teaching, I knew this was my calling to help
+        other people make positive changes in their lives through education. As
+        I said in the beginning, I believe if you are willing to put in the
+        work, you can accomplish anything in this life. If programming is what
+        you are hoping to learn and work hard on, I am happy to show you the
+        way!
+      </p>
 
       <h2>Some other facts about me</h2>
-      <p>I live just outside of Cleveland, Ohio with my amazing wife, Jen, our daughter Isabella. When I am not spending time with my family, I have a number of things that keep me busy. I love to read books, listen to podcasts and watch online courses. I have a real thirst for knowledge and it consumes me every single day of the week. I also love to run & lift heavy weights, which keep me balanced throughout the week.</p>
-      <p>I am also the president of a nonprofit here in Northeast Ohio called CLE Cares. Our mission is to try and help as many people as we can in the area and give back to a place that we hold near and dear to our hearts. I also love to speak at user group meetings and conferences around the country. If you are interested in having me speak at your event, please contact me.</p>
+      <p>
+        I live just outside of Cleveland, Ohio with my amazing wife, Jen, our
+        daughter Isabella. When I am not spending time with my family, I have a
+        number of things that keep me busy. I love to read books, listen to
+        podcasts and watch online courses. I have a real thirst for knowledge
+        and it consumes me every single day of the week. I also love to run &
+        lift heavy weights, which keep me balanced throughout the week.
+      </p>
+      <p>
+        I am also the president of a nonprofit here in Northeast Ohio called CLE
+        Cares. Our mission is to try and help as many people as we can in the
+        area and give back to a place that we hold near and dear to our hearts.
+        I also love to speak at user group meetings and conferences around the
+        country. If you are interested in having me speak at your event, please
+        contact me.
+      </p>
     </main>
   </Layout>
 </template>
