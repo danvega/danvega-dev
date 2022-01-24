5f34d7165567eb6580a3a6c30574452ac2cf8ebe
Dan Vega <danvega@gmail.com>
2022-01-24 10:09:36 -0500

    upgrading gridsome and rolling back blog post

--- gridsome.server.js
+++ gridsome.server.js
@@ -10,36 +10,36 @@ const path = require('path');
 const pick = require('lodash.pick');
 
 module.exports = function (api, options) {
-  api.loadSource(store => {
-    // Use the Data store API here: https://gridsome.org/docs/data-store-api
-  })
+  // api.loadSource(store => {
+  //   // Use the Data store API here: https://gridsome.org/docs/data-store-api
+  // })
 
-  api.beforeBuild(({ config, store }) => {
+  // api.beforeBuild(({ config, store }) => {
 
-    // Generate an index file for Fuse to search Posts
-    // const { collection } = store.getContentType('Post');
+  //   // Generate an index file for Fuse to search Posts
+  //   const { collection } = store.getContentType('Post');
 
-    // const posts = collection.data.map(post => {
-    //   return pick(post, ['title', 'path', 'excerpt']);
-    // });
+  //   const posts = collection.data.map(post => {
+  //     return pick(post, ['title', 'path', 'excerpt']);
+  //   });
 
-    // const output = {
-    //   dir: './static',
-    //   name: 'search.json',
-    //   ...options.output
-    // }
+  //   const output = {
+  //     dir: './static',
+  //     name: 'search.json',
+  //     ...options.output
+  //   }
 
-    // const outputPath = path.resolve(process.cwd(), output.dir)
-    // const outputPathExists = fs.existsSync(outputPath)
-    // const fileName = output.name.endsWith('.json')
-    //   ? output.name
-    //   : `${output.name}.json`
+  //   const outputPath = path.resolve(process.cwd(), output.dir)
+  //   const outputPathExists = fs.existsSync(outputPath)
+  //   const fileName = output.name.endsWith('.json')
+  //     ? output.name
+  //     : `${output.name}.json`
 
-    // if (outputPathExists) {
-    //   fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
-    // } else {
-    //   fs.mkdirSync(outputPath)
-    //   fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
-    // }
-  })
+  //   if (outputPathExists) {
+  //     fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
+  //   } else {
+  //     fs.mkdirSync(outputPath)
+  //     fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
+  //   }
+  // })
 }
