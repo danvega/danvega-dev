933e5191308b44b2249a9d39d90adcbf62286b5a
Dan Vega <danvega@gmail.com>
2022-01-24 09:53:09 -0500

    removing search index for now

--- gridsome.server.js
+++ gridsome.server.js
@@ -17,29 +17,29 @@ module.exports = function (api, options) {
   api.beforeBuild(({ config, store }) => {
 
     // Generate an index file for Fuse to search Posts
-    const { collection } = store.getContentType('Post');
+    // const { collection } = store.getContentType('Post');
 
-    const posts = collection.data.map(post => {
-      return pick(post, ['title', 'path', 'excerpt']);
-    });
+    // const posts = collection.data.map(post => {
+    //   return pick(post, ['title', 'path', 'excerpt']);
+    // });
 
-    const output = {
-      dir: './static',
-      name: 'search.json',
-      ...options.output
-    }
+    // const output = {
+    //   dir: './static',
+    //   name: 'search.json',
+    //   ...options.output
+    // }
 
-    const outputPath = path.resolve(process.cwd(), output.dir)
-    const outputPathExists = fs.existsSync(outputPath)
-    const fileName = output.name.endsWith('.json')
-      ? output.name
-      : `${output.name}.json`
+    // const outputPath = path.resolve(process.cwd(), output.dir)
+    // const outputPathExists = fs.existsSync(outputPath)
+    // const fileName = output.name.endsWith('.json')
+    //   ? output.name
+    //   : `${output.name}.json`
 
-    if (outputPathExists) {
-      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
-    } else {
-      fs.mkdirSync(outputPath)
-      fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
-    }
+    // if (outputPathExists) {
+    //   fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
+    // } else {
+    //   fs.mkdirSync(outputPath)
+    //   fs.writeFileSync(path.resolve(process.cwd(), output.dir, fileName), JSON.stringify(posts))
+    // }
   })
 }
