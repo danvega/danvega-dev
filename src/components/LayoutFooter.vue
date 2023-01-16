b61a05560d90bfda8a2e132fefcfb39c4db61729
Dan Vega <danvega@gmail.com>
2023-01-16 15:22:23 -0500

    removing the footer links, didn't add much value and didn't like how it looked

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -4,71 +4,25 @@
       <div class="social-links">
         <p>Made with ❤️ in Cleveland, OH.</p>
         <!-- https://developers.google.com/web/tools/lighthouse/audits/noopener -->
-        <a
-          href="http://twitter.com/therealdanvega"
-          aria-label="twitter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="http://twitter.com/therealdanvega" aria-label="twitter" target="_blank" rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'twitter']" />
         </a>
-        <a
-          href="https://github.com/danvega/"
-          aria-label="github"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://github.com/danvega/" aria-label="github" target="_blank" rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'github']" />
         </a>
-        <a
-          href="http://www.youtube.com/danvega"
-          aria-label="youtube"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="http://www.youtube.com/danvega" aria-label="youtube" target="_blank" rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'youtube']" />
         </a>
-        <a
-          href="https://www.reddit.com/u/therealdanvega"
-          aria-label="reddit"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://www.reddit.com/u/therealdanvega" aria-label="reddit" target="_blank" rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'reddit']" />
         </a>
-        <a
-          href="https://www.instagram.com/thedanvega/"
-          aria-label="instagram"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://www.instagram.com/thedanvega/" aria-label="instagram" target="_blank"
+          rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'instagram']" />
         </a>
-        <a
-          href="https://www.linkedin.com/in/danvega/"
-          aria-label="linkedin"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://www.linkedin.com/in/danvega/" aria-label="linkedin" target="_blank" rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'linkedin']" />
         </a>
-      </div>
-
-      <div class="footer-links">
-        <ul>
-          <li><a href="/" aria-label="Home">Home</a></li>
-          <li><a href="/blog" aria-label="Blog">Blog</a></li>
-          <li><a href="/courses" aria-label="Courses">Courses</a></li>
-        </ul>
-        <ul>
-          <li><a href="/newsletter" aria-label="Newsletter">Newsletter</a></li>
-          <li><a href="/speaking" aria-label="Speaking">Speaking</a></li>
-          <li><a href="/uses" aria-label="Uses">Uses</a></li>
-        </ul>
-        <ul>
-          <li><a href="/about" aria-label="About">About</a></li>
-          <li><a href="/contact" aria-label="Contact">Contact</a></li>
-        </ul>
       </div>
 
     </div>
@@ -101,7 +55,7 @@ footer a:hover {
   color: #e6e6e6;
 }
 
-footer a > svg {
+footer a>svg {
   fill: white;
   width: 1.2rem;
 }
@@ -109,20 +63,9 @@ footer a > svg {
 footer .social-links {
   display: flex;
 }
-.social-links p {
-  margin:8px;
-}
-footer .footer-links {
-  display: flex;
-}
 
-.footer-links ul {
-  list-style-type: none;
-  margin:0 40px 0 0;
-  padding:0px;
-}
-.footer-links li {
-  padding:8px;
+.social-links p {
+  margin: 8px;
 }
 
 @media (max-width: 1024px) {
@@ -130,8 +73,9 @@ footer .footer-links {
     padding: 0 20px;
   }
 }
+
 @media (max-width: 600px) {
-  footer > .container {
+  footer>.container {
     flex-direction: column;
   }
 }
