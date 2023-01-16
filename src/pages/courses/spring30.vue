9ba06ad4160082ecbd7a0365dc8f256aa102ccc5
Dan Vega <danvega@gmail.com>
2023-01-16 15:19:29 -0500

    Start of a new idea 💡

---
+++ src/pages/courses/spring30.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+    <h1>
+      Learn Spring in 30 days
+    </h1>
+    <p>In this course, you will learn the Spring Framework by building a real-world application over the course of 30
+      days. You will start with the basics of the framework, including dependency injection and bean configuration, and
+      progress to more advanced topics such as Spring MVC, data access with Spring Data, and security.
+    </p>
+    <p>By the end of the
+      course, you will have a solid understanding of the Spring Framework and the ability to build robust and scalable
+      applications using it. This course is designed for experienced Java developers looking to enhance their skills and
+      build web applications with Spring Framework.</p>
+
+    <br /><br />
+    <convert-kit uid="b0915be2cf" script="https://danvega.ck.page/b0915be2cf/index.js" />
+  </Layout>
+
+</template>
+
+<script>
+import ConvertKit from "@/components/ConvertKit.vue";
+
+export default {
+  components: {
+    ConvertKit
+  }
+};
+</script>
