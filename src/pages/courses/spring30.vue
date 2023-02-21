9675316041185ec3e3e27417ce68824ddd51da83
Dan Vega <danvega@gmail.com>
2023-02-21 18:53:33 -0500

    Learn Spring in 30 days landing page

--- src/pages/courses/spring30.vue
+++ src/pages/courses/spring30.vue
@@ -3,19 +3,86 @@
     <h1>
       Learn Spring in 30 days
     </h1>
-    <p>In this course, you will learn the Spring Framework by building a real-world application over the course of 30
-      days. You will start with the basics of the framework, including dependency injection and bean configuration, and
-      progress to more advanced topics such as Spring MVC, data access with Spring Data, and security.
+    <p>
+      This 30-day course teaches you how to build and deploy a real-world Java application using Spring Framework. You'll
+      learn to build an app with Spring Boot and popular Spring projects like Spring MVC, Spring Data, and Spring
+      Security. By the end of the course, you'll deploy your completed app to production.
     </p>
-    <p>By the end of the
-      course, you will have a solid understanding of the Spring Framework and the ability to build robust and scalable
-      applications using it. This course is designed for experienced Java developers looking to enhance their skills and
-      build web applications with Spring Framework.</p>
 
-    <br /><br />
     <convert-kit uid="b0915be2cf" script="https://danvega.ck.page/b0915be2cf/index.js" />
-  </Layout>
 
+    <h2>Course Outline</h2>
+    <p>Here is the course outline:</p>
+    <ul>
+      <li>Week 01: Getting Started</li>
+      <li>Week 02: Spring MVC</li>
+      <li>Week 03: Spring Data</li>
+      <li>Week 04: Spring Security</li>
+      <li>Week 05: Testing</li>
+      <li>Week 06: Production</li>
+    </ul>
+    <p>Please provide feedback on the structure and flow of the course. I aim to ensure the material is presented in a
+      clear, logical, and engaging manner.</p>
+    <h3>Week 0</h3>
+    <ul>
+      <li>About this course</li>
+    </ul>
+    <h3>Week 1</h3>
+    <ul>
+      <li>Day 01: What is Spring</li>
+      <li>Day 02: Creating a Spring Boot Project</li>
+      <li>Day 03: Structuring your Code</li>
+      <li>Day 04: Beans & Application Context</li>
+      <li>Day 05: Dependency Injection</li>
+      <li>Week 1 Exercise</li>
+    </ul>
+    <h3>Week 2</h3>
+    <ul>
+      <li>Day 06: Spring MVC Intro</li>
+      <li>Day 07: Spring MVC READ</li>
+      <li>Day 08: Spring MVC Create / Update / Delete</li>
+      <li>Day 09: Data Validation</li>
+      <li>Day 10: CORS</li>
+      <li>Week 2 Exercise</li>
+    </ul>
+    <h3>Week 3</h3>
+    <ul>
+      <li>Day 11: Spring Data Intro</li>
+      <li>Day 12: Configuring a Datasource</li>
+      <li>Day 13: JDBC Template</li>
+      <li>Day 14: Spring Data</li>
+      <li>Day 15: Spring Data Repositories</li>
+      <li>Week 3 Exercise</li>
+    </ul>
+    <h3>Week 4</h3>
+    <ul>
+      <li>Day 16: Spring Security Introduction</li>
+      <li>Day 17: Spring Security Config</li>
+      <li>Day 18: User Details Service</li>
+      <li>Day 19: JSON Web Tokens (JWT)</li>
+      <li>Day 20: Client + Server 🤝</li>
+      <li>Week 4 Exercise</li>
+    </ul>
+    <h3>Week 5</h3>
+    <ul>
+      <li>Day 21: Testing Pyramid</li>
+      <li>Day 22: Unit Testing</li>
+      <li>Day 23: Integration Testing</li>
+      <li>Day 24: Slice Tests</li>
+      <li>Day 25: Testcontainers</li>
+      <li>Week 5 Exercise</li>
+    </ul>
+    <h3>Week 6</h3>
+    <ul>
+      <li>Day 26: Configuration</li>
+      <li>Day 27: Profiles</li>
+      <li>Day 28: Actuator</li>
+      <li>Day 29: Building for Production</li>
+      <li>Day 30: Production - The greatest place on the web</li>
+      <li>Week 6 Exercise</li>
+    </ul>
+
+  </Layout>
 </template>
 
 <script>
