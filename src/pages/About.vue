1ab50bd389849e7a6678fa957b7d448a4cd746e2
Dan Vega <danvega@gmail.com>
2023-03-07 11:54:28 -0500

    Updating about me page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,11 +2,7 @@
   <Layout>
     <main>
       <h1>Hi, I’m Dan Vega.</h1>
-      <img
-        src="../assets/img/dv-headshot.jpg"
-        alt="Dan Vega"
-        class="headshot"
-      />
+      <img src="../assets/img/dv-headshot.jpg" alt="Dan Vega" class="headshot" />
       <p>
         I am a Software Engineer living just outside of one of my favorite
         places in the world, Cleveland, Ohio. I am a self-taught programmer who
@@ -17,7 +13,7 @@
       <p>
         I have been writing software for over 20+ years now and I use my
         knowledge, passion, and influence to help other developers achieve their
-        goals. I teach online and currently, have over 150,000 students.
+        goals. I teach online and currently, have over 160,000 students.
       </p>
       <p>
         In my personal life I am a husband to my best friend in the world and a
@@ -26,63 +22,54 @@
       </p>
       <h2>My Journey</h2>
       <p>
-        My journey to becoming a computer programmer did not start out at a
-        fancy university. I like to give my mom some credit for making me take a
-        typing class in high school that I excelled in, which sparked my
-        interest in computers, thanks, mom! After high school, I was working as
-        a landscaper and one day I just decided that this was not the life for
-        me. I decided to enroll at a local college for a programming
-        certification in Applied Computer Sciences. Once I completed this
-        certification, I did some freelancing to hone my skills, and soon after
-        I landed my first job at a startup in San Francisco! It was difficult
-        leaving my friends and family, but I was so excited for this opportunity
-        and quickly packed my bags for the west coast.
+        My journey to becoming a computer programmer did not start at a fancy university. Thanks to my mom, who made me
+        take a typing class in high school, in which I excelled, I became interested in computers. After high school, I
+        worked as a landscaper. One day, I decided that this was not life for me. I chose to enroll at a local college for
+        a programming certification in Applied Computer Sciences. Once I completed this certification, I did some
+        freelancing to hone my skills, and soon after, I landed my first job at a startup in San Francisco! It was
+        difficult leaving my friends and family, but I was so excited about this opportunity and quickly packed my bags
+        for the West Coast.
       </p>
       <p>
-        My educational journey did not stop there. I like to consider myself a
-        lifelong learner. I believe having a passion for learning really is the
-        secret to success in life. While I was living in California, I hired a
-        teaching assistant from UC Berkeley to tutor me in computer programming.
-        I spent most of my nights far from home immersing myself in Java
-        programming books to advance my skills, and it certainly paid off!
+        My educational journey did not stop there. I consider myself a lifelong learner. Having a passion for learning is
+        the secret to success in life. While I was living in California, I hired a teaching assistant from UC Berkeley to
+        tutor me in computer programming. I spent most of my nights far from home immersing myself in Java programming
+        books to advance my skills, which certainly paid off!
       </p>
       <p>
-        After two years in San Francisco, I landed a new position back in
-        Cleveland and jumped at the opportunity to move back home. I continued
-        to advance to different positions and companies over the years and here
-        I am, 20+ years later, still loving to code every single day.
+        After two years in San Francisco, I landed a new position back in Cleveland and jumped at the opportunity to move
+        back home. I continued to advance to different positions and companies over the years, and here I am, 20+ years
+        later, still loving to code every single day.
       </p>
 
       <h2>One thing that has always stuck with me</h2>
       <p>
-        One thing that has always stuck with me over the years is how grateful I
-        am for all of the people who taught and inspired me over the years. This
-        help inspired me to always share my passion for learning with others.
-        When I discovered online teaching, I knew this was my calling to help
-        other people make positive changes in their lives through education. As
-        I said in the beginning, I believe if you are willing to put in the
-        work, you can accomplish anything in this life. If programming is what
-        you are hoping to learn and work hard on, I am happy to show you the
-        way!
+        One thing that has always stuck with me over the years is how grateful I am for all of the people who taught and
+        inspired me. This helped inspire me to share my passion for learning with others. When I discovered online
+        teaching, I knew this was my calling to help other people make positive changes in their lives through education.
+        As I said in the beginning, if you are willing to put in the work, you can accomplish anything in this life. If
+        programming is what you are hoping to learn and work hard on, I am happy to show you the way!
       </p>
 
       <h2>Some other facts about me</h2>
       <p>
-        I live just outside of Cleveland, Ohio with my amazing wife, Jen, our
-        daughter Isabella. When I am not spending time with my family, I have a
-        number of things that keep me busy. I love to read books, listen to
-        podcasts and watch online courses. I have a real thirst for knowledge
-        and it consumes me every single day of the week. I also love to run &
-        lift heavy weights, which keep me balanced throughout the week.
+        I live just outside of Cleveland, Ohio with my wife Jen, and our 2 daughters Isabella and Juliana. When not
+        spending time with my family, I love reading books, listening to podcasts, and watching online courses. I have a
+        real thirst for knowledge and it consumes me every day of the week. I also love to run and lift heavy weights,
+        which keeps me balanced throughout the week.
       </p>
       <p>
-        I am also the president of a nonprofit here in Northeast Ohio called CLE
-        Cares. Our mission is to try and help as many people as we can in the
-        area and give back to a place that we hold near and dear to our hearts.
-        I also love to speak at user group meetings and conferences around the
-        country. If you are interested in having me speak at your event, please
-        contact me.
+        I am also the president of a nonprofit organization here in Northeast Ohio called CLE Cares. Our mission is to
+        help as many people as we can in the area and give back to a place that we hold near and dear to our hearts. I
+        also love to speak at user group meetings and conferences around the country. If you are interested in having me
+        speak at your event, please contact me.
       </p>
+
+      <h2>Contact Me</h2>
+      <p>If you are having difficulty with a new language, framework, or tool, know that even those of us who have been in
+        the field for 20 years experience the same struggles. If you need assistance with a particular issue, please don't
+        hesitate to reach out. While I may not be able to help everyone, I will certainly do my best and, at the very
+        least, point you in the right direction.</p>
     </main>
   </Layout>
 </template>
