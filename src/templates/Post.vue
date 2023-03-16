a0a2abcb93392529772da52235d33c625f7060b7
Dan Vega <danvega@gmail.com>
2023-03-16 09:32:28 -0400

    Updating bio on the post template

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,24 +1,17 @@
 <template>
   <Layout>
     <h1>{{ $page.post.title }}</h1>
-    <p class="postDateTime">Published On: {{ formatCreatedOn }} <span v-if="$page.post.updatedOn">| Updated On:{{ formatDate($page.post.updatedOn) }}</span> • ☕️ {{ $page.post.timeToRead }} min read</p>
+    <p class="postDateTime">Published On: {{ formatCreatedOn }} <span v-if="$page.post.updatedOn">| Updated On:{{
+      formatDate($page.post.updatedOn) }}</span> • ☕️ {{ $page.post.timeToRead }} min read</p>
     <div class="embed-container" v-if="$page.post.video">
-      <iframe
-        :src="$page.post.video"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-        v-if="$page.post.video"
-      ></iframe>
+      <iframe :src="$page.post.video" frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
+        v-if="$page.post.video"></iframe>
     </div>
-    <g-image
-      v-if="!$page.post.video && $page.post.cover"
-      :src="$page.post.cover"
-      class="cover"
-      :alt="$page.post.title"
-    />
+    <g-image v-if="!$page.post.video && $page.post.cover" :src="$page.post.cover" class="cover" :alt="$page.post.title" />
 
-    <div v-if="$page.post.video" class="callout">💡 The video tutorial for this blog post can be found above or you can <a :href="getCalloutUrl($page.post.video)">click here</a> to watch it on YouTube.</div>
+    <div v-if="$page.post.video" class="callout">💡 The video tutorial for this blog post can be found above or you can <a
+        :href="getCalloutUrl($page.post.video)">click here</a> to watch it on YouTube.</div>
 
     <article v-html="$page.post.content" class="article" />
     <blog-tag :tags="$page.post.tags" />
@@ -39,7 +32,7 @@
       <div class="bio">
         <h3>Dan Vega</h3>
         <p>
-          I’m a Husband, Father, Curriculum Developer and maker of things from
+          I’m a Husband, Father, Spring Developer Advocate and maker of things from
           Cleveland Ohio. I created this website as a place to document my
           journey as I learn new things and share them with you. I have a real
           passion for teaching and I hope that one of blog posts, videos or
@@ -91,7 +84,7 @@ export default {
       title: this.$page.post.title,
       meta: [
         { name: "description", content: this.$page.post.excerpt },
-        { name: "keywords", content: this.$page.post?.keywords},
+        { name: "keywords", content: this.$page.post?.keywords },
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:description", content: this.$page.post.excerpt },
         { name: "twitter:title", content: this.$page.post.title },
@@ -128,7 +121,7 @@ export default {
     },
     getCalloutUrl(url) {
       const parts = url.split("/");
-      const id = parts[parts.length-1];
+      const id = parts[parts.length - 1];
       return "https://youtu.be/" + id;
     }
   },
@@ -165,47 +158,59 @@ export default {
 h1 {
   margin: 0;
 }
+
 .postDateTime {
   margin: 0 0 5px 0;
   font-size: 0.9rem;
 }
+
 .cover {
   margin-top: 10px;
   max-width: 100%;
 }
+
 .article {
   margin-top: 20px;
 }
+
 .article img {
   max-width: 100%;
 }
+
 .fas.fa-hashtag {
-  display: none; /* hides the hashtag next to each heading */
+  display: none;
+  /* hides the hashtag next to each heading */
 }
+
 .shiki-inline {
   background: var(--shiki-inline-background) !important;
   padding: 2px;
   color: var(--shiki-inline-color) !important;
   font-size: 1.1rem;
 }
-p > code {
+
+p>code {
   background: var(--shiki-inline-background) !important;
   padding: 2px;
   color: var(--shiki-inline-color) !important;
   font-size: 0.9rem;
 }
+
 .twitter-tweet {
   margin: 10px auto;
 }
+
 code {
   font-family: hack, sans-serif;
   font-weight: 400;
   font-style: normal;
   width: 100%;
 }
+
 .shiki {
   padding: 1rem;
 }
+
 .article .fa-youtube {
   color: red;
   margin: 0 5px 0 0;
@@ -217,6 +222,7 @@ code {
   padding-top: 56.25%;
   height: 0;
 }
+
 .embed-container iframe,
 .embed-container object,
 .embed-container embed {
@@ -233,31 +239,38 @@ code {
   background-color: rgb(242, 242, 242);
   padding: 10px;
 }
+
 .followme a:link,
 .followme a:visited {
   color: var(--bright-blue);
 }
+
 .author {
   display: flex;
   margin: 40px 0;
 }
+
 .author p {
   margin: 0;
   padding: 0;
 }
+
 .author h3 {
   font-size: 1.8rem;
   border-bottom: 1px solid #f2f3f3;
   padding-bottom: 5px;
 }
+
 .bio {
   padding: 0 30px;
 }
+
 .avatar img {
   border-radius: 50%;
   padding: 5px;
   border: 2px solid #eeeeee;
 }
+
 #convertkit {
   margin: 15px;
 }
@@ -272,11 +285,10 @@ th {
   font-weight: bold;
 }
 
-#table-of-contents {
-}
+#table-of-contents {}
 
 .article li,
-.article li > p {
+.article li>p {
   margin: 0;
   padding: 0;
   margin-block-start: 0;
@@ -284,15 +296,17 @@ th {
   line-height: 1.5;
 }
 
-@media (max-width: 1024px) {
-}
+@media (max-width: 1024px) {}
+
 @media (max-width: 768px) {
   .author {
     flex-direction: column;
   }
+
   .bio p {
     font-size: 1rem;
   }
+
   .author .avatar {
     order: 2;
     align-self: center;
@@ -305,6 +319,7 @@ th {
     margin: 0;
     padding: 0;
   }
+
   pre.shiki {
     font-size: 0.8rem;
     max-width: 89vw;
@@ -312,7 +327,8 @@ th {
   }
 }
 
-section.tldr, section.promo {
+section.tldr,
+section.promo {
   border: 1px solid #eee;
   padding: 20px;
   font-family: "Roboto Slab", serif;
@@ -320,30 +336,35 @@ section.tldr, section.promo {
   font-weight: 300;
   font-size: 1.1rem;
 }
+
 section.tldr {
   border-color: var(--bright-blue);
   color: var(--font-color);
   font-size: 1rem;
 }
+
 section.promo {
   border: 1px dashed var(--bright-blue);
 }
+
 section.promo small {
   font-size: 0.99rem;
 }
+
 section.promo h3 {
-  margin:0px;
-  padding:0px;
+  margin: 0px;
+  padding: 0px;
 }
+
 section.promo.tip-jar {
   text-align: center;
-  color:var(--font-color);
+  color: var(--font-color);
 }
 
 iframe {
-	aspect-ratio: 16 / 9;
-	height: 100%;
-	width: 100%;
+  aspect-ratio: 16 / 9;
+  height: 100%;
+  width: 100%;
 }
 
 .callout {
